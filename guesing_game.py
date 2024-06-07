07e08d853c92e8ae2b794b13896b8b01d88eb8ad
unknown <benptorrance@gmail.com>
2024-06-07 15:57:27 -0400

    Updated comments in the code. Removed test word used for troubleshooting. Optimized the program slightly by getting rid of a redundant array.

--- guesing_game.py
+++ guesing_game.py
@@ -2,19 +2,18 @@ import random
 from colorama import Fore
 
 def choose_word():
-    #chooses a random number and uses that to select a word. The range is determined by the length of the list and the chosen word is passed back out.
-    r_num = random.randint(0,len(word_list))
+    #Generates a random number and uses that to select a word. The range is determined by the length of the list and the chosen word is passed back out.
+    r_num = random.randint(0,len(word_list)-1)
     chosen_word = word_list[r_num]
-    chosen_word = "tests"
     return chosen_word
 
 def guessing_method(c_word):
-    #Will be used later but these are the lists that will contain the correct letters guessed by the player. I may have found a way to ignore this but it may just be
-    #due to how the program is currently written. The loop might have issues displaying the old guesses if it isn't saved to a list.
-    #One of the changes will be to move over to a message box to display the colors on the letters.
+    #Declaring the array that will be used to track the wrong numbers and the dictionary that will track the misplaced letters and the number of times each letter appears.
+    #A dictionary was used for the letters that are misplaced for the key:value pair that dictionaries offer.
+    #The value connected to each letter tracks the number of misplaced letters to prevent a letter from disappearing if
+    #only one is in the correct place.
     wrong_letters = []
-    misplaced_letters = []
-    num_of_misplaced = {}
+    misplaced_letters = {}
 
     #Variables to keep track of the number of turns and the current turn as well as a variable for the user's guess itself.
     maximum_turns = 5
@@ -38,22 +37,24 @@ def guessing_method(c_word):
                 if guess[i] == c_word[i]:
                     print(Fore.GREEN + guess[i]+ Fore.RESET, end="")
                     if guess[i] in misplaced_letters:
-                        misplaced_letters.remove(guess[i])
+                        if misplaced_letters[guess[i]] > 1:
+                            misplaced_letters[guess[i]] -= 1
+                        if misplaced_letters[guess[i]] == 0:
+                            misplaced_letters[guess[i]].remove(guess[i])
                     i += 1
-                #If its the same but not in the correct position then it is yellow and adds it to the list of misplaced letters
+                #If its the same but not in the correct position then it is yellow and adds it to the list of misplaced letters.
                 elif guess[i] in c_word and guess[i] != c_word[i]:
                     print(Fore.YELLOW + guess[i] + Fore.RESET, end="")
-                    letter_count = c_word.count(guess[i])
+
+                    #Checks if a letter is present in the list of misplaced_letters. If it isn't, then a key is created with its name and the value is set to 1.
                     if guess[i] not in misplaced_letters:
-                        misplaced_letters.append(guess[i])
-                    if guess[i] in misplaced_letters and guess[i] not in num_of_misplaced:
-                        num_of_misplaced[guess[i]] = 1
-                        print(str(num_of_misplaced[guess[i]]))
-                    elif guess[i] in misplaced_letters and guess[i] in num_of_misplaced:
-                        if num_of_misplaced[guess[i]] < letter_count:
-                            num_of_misplaced[guess[i]] = num_of_misplaced[guess[i]] +1
-                            print(str(num_of_misplaced[guess[i]]))
+                        misplaced_letters[guess[i]] = 1
+                    #If it is present and the letter's value is still below the number of times it appears in the word, then it is incremented by 1. 
+                    elif guess[i] in misplaced_letters:
+                        if misplaced_letters[guess[i]] < c_word.count(guess[i]):
+                            misplaced_letters[guess[i]] += 1
                     i += 1
+
                 #Otherwise it is grey and it adds the letter to the list of letters not present in the word.
                 else: 
                     print(Fore.WHITE + guess[i] + Fore.RESET, end="")
@@ -61,15 +62,14 @@ def guessing_method(c_word):
                         wrong_letters.append(guess[i])
                     i += 1  
 
-            #Increments the current turn and reports that the user made an incorrect guess
+            #Increments the current turn and reports that the user made an incorrect guess.
             current_turn +=1
             print("\n"+"Incorrect Guess. Try again.")
             print("You have "+str(maximum_turns - current_turn)+" Guesses left.")
-            print(num_of_misplaced)
 
             #Prints the list of Misplaced letters in the word and the list of guessed letters that aren't in the word so the user can keep track of previous guesses better.
             print("Misplaced Letters: ", end="")
-            print(misplaced_letters)
+            print(list(misplaced_letters.keys()))
             print("Wrong Letters: ", end="")
             print(wrong_letters)
 
